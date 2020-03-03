d3f4c97ddc951f9c0681b80088664b28cad082e9
DrInnovatio <turing1723@gmail.com>
2020-03-03 10:01:38 +1000

    Added services

---
+++ src/components/Services.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Service extends Component{
+  render(){
+    return(
+      <div className="services">
+          <h3>Services</h3>
+          <h2>What we offer</h2>
+      </div>
+    )
+  }
+}
+
+export default Service;
+
